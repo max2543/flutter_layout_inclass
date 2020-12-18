dc25d6357e8613064564f50658afee2f5c121652
max2543 <62122660102@g.lpru.ac.th>
2020-12-18 16:32:34 +0700

    update row_widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_layout_inclass/center_widget_layout.dart';
 import 'package:flutter_layout_inclass/container_widget_layout.dart';
+import 'package:flutter_layout_inclass/row_widget_layout.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,8 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.purple),
-      home: ContainerWidgetPage(),
+      home: RowWidgetPage(),
     );
   }
 }
